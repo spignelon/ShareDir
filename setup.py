86ed761b55fa21466b93f955da814b2bbb232e1a
Ujjawal Saini <spignelon@protonmail.com>
2024-10-22 15:46:53 +0530

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sharedir',
-    version='0.1',
+    version='1.0',
     description='A simple tool to share files and directories over LAN or internet.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
